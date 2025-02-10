40cdbf758263bd340667a7a725c70cdf224be6c8
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2025-02-10 15:05:49 -0300

    criando componetização

--- src/app/pages/cadastro/cadastro.module.ts
+++ src/app/pages/cadastro/cadastro.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CadastroRoutingModule } from './cadastro-routing.module';
+import { FormsModule } from '@angular/forms'; // aqui eu importei o formulário
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CadastroRoutingModule
+    CadastroRoutingModule,
+    FormsModule  // aqui habilitei o formulário no modulo
   ]
 })
 export class CadastroModule { }
