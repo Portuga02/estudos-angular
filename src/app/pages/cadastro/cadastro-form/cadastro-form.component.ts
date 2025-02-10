3f9b756c0a52cfab045e3e3363e2299b74714016
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2025-02-10 14:31:06 -0300

    iniciando estudos em angular

---
+++ src/app/pages/cadastro/cadastro-form/cadastro-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-form',
+  imports: [],
+  templateUrl: './cadastro-form.component.html',
+  styleUrl: './cadastro-form.component.css'
+})
+export class CadastroFormComponent {
+
+}
